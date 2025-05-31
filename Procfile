8f031c969b26698f492e82ee08f5f16c9d05e8c4
Shivaay <shivaay20005@gmail.com>
2025-05-31 11:33:08 +0530

    Function Updated

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: python server.py
+web: python web_server.py
 worker: python -m Extractor
--- keep_alive.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "✅ Worker running. No frontend available."
-
-if __name__ == "__main__":
-    app.run()
