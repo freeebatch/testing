06af28d6fe7b86efd7ca0e598885ec61cf8bf93a
freeebatch <koushalanurag0@gmail.com>
2025-06-10 06:23:12 +0530

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -7,4 +7,4 @@ def home():
     return "Bot is running..."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
